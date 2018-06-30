dff8c2efe36660e5e7e46fb0d685cf1337e9051b
Yong Do <yong.do@korbit.co.kr>
2018-06-30 23:13:37 +0900

    action returns an array of exchange to display on the Dashboard.

--- src/actions.js
+++ src/actions.js
@@ -9,16 +9,60 @@ const showExchangeTicker = (responses) => {
 
 export const loadAllExchangeTicker = (currency) => {
   const exchanges = _.keysIn(exchangeApi);
-    return (dispatch) => {
-      return Promise.map(exchanges, (exchange) => {
-        return exchangeApi[exchange](currency)
-        .then((response) => {
-          return response;
-        });
-      })
-      .then((responses) => {
-        console.log(`responses=${JSON.stringify(responses)}`);
-        dispatch(showExchangeTicker(responses));
+  return (dispatch) => {
+    return Promise.map(exchanges, (exchange) => {
+      return exchangeApi[exchange](currency)
+      .then((response) => {
+        return response;
       });
-    };
+    })
+    .then((responses) => {
+      Promise.all(responses).then(function(items) {
+        let arr = [];
+        for (let item of items) {
+          var ticker = {};
+          ticker.exchange = item.exchange;
+
+          let data = JSON.parse(item.response);
+          switch (ticker.exchange) {
+            case 'korbit':
+            ticker.price = Number.parseInt(data.last);
+            ticker.volume = Number.parseInt(data.volume);
+            break;
+
+            case 'bithumb':
+            ticker.price = Number.parseInt(data.data.average_price);
+            ticker.volume = Number.parseInt(data.data.volume_1day);
+            break;
+
+            case 'coinone':
+            ticker.price = Number.parseInt(data.last);
+            ticker.volume = Number.parseInt(data.volume);
+            break;
+
+            case 'bitstamp': {
+              let price = Number.parseInt(data.bid) +
+              Number.parseInt(data.ask);
+              price /= 2;
+              ticker.price = price;
+              ticker.volume = data.volume;
+            }
+            break;
+
+            case 'okcoin':
+            let price = (Number.parseInt(data.ticker.buy) +
+            Number.parseInt(data.ticker.sell)) / 2;
+            ticker.price = price;
+            ticker.volume = data.ticker.vol;
+            break;
+          }
+
+          arr.push(ticker);
+        }
+
+        console.log(`arr: ${JSON.stringify(arr)}`);
+        dispatch(showExchangeTicker(arr));
+      });
+    });
+  };
 };
