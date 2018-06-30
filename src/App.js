31ab09d09cc72a46f379cb257d6d5b14d2707882
Yong Do <yong.do@korbit.co.kr>
2018-06-30 18:24:30 +0900

    issue in actions

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Dashboard from './Dashboard';
+import * as actions from './actions';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.loadExchangeActions.loadAllExchangeTicker('btc');
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,4 +23,10 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return {
+    loadExchangeActions: bindActionCreators(actions, dispatch)
+  };
+}
+
+export default connect(undefined, mapDispatchToProps)(App);
